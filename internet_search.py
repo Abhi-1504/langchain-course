d6107801c9f370d3e864413268f8be253b53eadf
Abhilash Raj <abhilash.raj.me@gmail.com>
2025-10-30 20:02:37 +0530

    ADD: ReAct Agent with Internet Search tool

---
+++ internet_search.py
@@ -0,0 +1,38 @@
+import os
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_classic import hub
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model='gpt-4')
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm,
+    tools=tools,
+    prompt=react_prompt
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
+def main():
+    
+    #llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details"
+            }
+        )
+    
+    print(result)
+
+if __name__ == "__main__":
+    main()
